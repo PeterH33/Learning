35c5c44cac9b48d871bef15b04ec984433879dd6
Peter Hartnett <peter.hartnett@gmail.com>
2024-06-12 16:09:44 -0400

    php vars

--- Processor.php
+++ Processor.php
@@ -8,15 +8,31 @@
    /* This line is just what is used in class to show that the submit works properly and you are getting input sent over properly
    */
     print_r($_POST);
-$gender = $_POST['gender'];
-$fName = $_POST['fName'];
-$lName = $_POST['lName'];
-$num1 = $_POST['num1'];
-$num2 = $_POST['num2'];
-$op = $_POST['op'];
+    //input
 
-print"<br>Hello $fName $lName, $gender, $num1, $num2, $op<br>";
+    $gender = $_POST['gender'];
+    $fName = $_POST['fName'];
+    $lName = $_POST['lName'];
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $op = $_POST['op'];
 
+    //Processing
+    if($op == '+')
+        $result = $num1 + $num2;
+    if($op == '-')
+        $result = $num1 - $num2;
+    if($op == '*')
+        $result = $num1 * $num2;
+    if($op == '/')
+        $result = $num1 / $num2;
+
+
+
+    //output
+    // print"<br>Hello $fName $lName, $gender, $num1, $num2, $op<br>";
+    print"<br>Hello $fName $lName<br>";
+    print"<br>$num1 $op $num2 = $result";
 ?>
 
 </p>
