dc06a172503b09623ba610270fad442f3e09b5f7
Peter Hartnett <peter.hartnett@gmail.com>
2024-06-12 16:01:04 -0400

    php vars

--- Processor.php
+++ Processor.php
@@ -7,10 +7,17 @@
     <?php
    /* This line is just what is used in class to show that the submit works properly and you are getting input sent over properly
    */
-  print_r($_POST)
+    print_r($_POST)
+$gender = $_POST['gender'];
+$fName = $_POST['fName'];
+$lName = $_POST['lName'];
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$op = $_POST['op'];
 
+print"Hello $fName $lName <br>";
 
-    ?>
+?>
 
 </p>
 </html>
